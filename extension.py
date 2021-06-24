8e34e6faa0867a518f5a3ae9e501e604c606dc90
TETALI JAIPAL REDDY <65228026+jaipalreddytetali@users.noreply.github.com>
2021-06-24 10:37:40 +0530

    created filename.py
    code to find out the extension of file

---
+++ extension.py
@@ -0,0 +1,4 @@
+#code to find the extension of the file 
+filename = input("Input the Filename: ")
+file_extension = filename.split(".")
+print("The extension of the file is : " + file_extension[-1])
