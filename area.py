42770c1ef0996b2a402358a85fbb6d98d90d8369
TETALI JAIPAL REDDY <65228026+jaipalreddytetali@users.noreply.github.com>
2021-06-24 10:36:18 +0530

    created area.py
    code to find out the area of circle

---
+++ area.py
@@ -0,0 +1,5 @@
+#Code to find area of circle
+from math import pi
+r=float(input('Input the radius of the circle :'))
+area=pi*r*r
+print("The area of the circle with radius " + str(r) + " is: " + str(area));
